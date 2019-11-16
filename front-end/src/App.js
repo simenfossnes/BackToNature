2e350942f154f539b63358c22bfce2405842f8a0
Simen Fossnes <simenf13@student.uia.no>
2019-11-16 19:11:34 +0100

    feat: 🎸 Added nav menu to the bottom; with animations

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Redirect, Switch, Route, Link } from "react-router-dom";
-import Profile from './screens/Profile';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route
+} from "react-router-dom";
+import Profile from "./screens/Profile";
 import Chances from "./screens/Chances";
-import Feed from './screens/Feed';
-import Onboarding from './components/Onboarding';
-
-const Menu = () => {
-  return (
-    <ul>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-      <li>
-        <Link to="/chances">Chances</Link>
-      </li>
-      <li>
-        <Link to="/feed">Feed</Link>
-      </li>
-    </ul>
-  );
-};
+import Feed from "./screens/Feed";
+import Onboarding from "./components/Onboarding";
+import NavMenu from "./components/NavMenu";
 
 const App = props => {
-
   const onBoarded = true;
 
   return (
-    <Router basename={process.env.PUBLIC_URL>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
-        <Menu />
+        <NavMenu />
         <Switch>
           <Route exact path="/">
             {onBoarded ? <Redirect to="/chances" /> : <Onboarding />}
