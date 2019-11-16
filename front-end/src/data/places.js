89afba181b9e7e43fa6c9f6f6988f2afc43ab9b2
Neža Đukić <neza.djukic@gmail.com>
2019-11-16 23:39:33 +0200

    feat: 🎸 All trails covered
    Added all trails, now startting with the color code, proper zoom and new
    dots.

---
+++ front-end/src/data/places.js
@@ -0,0 +1,662 @@
+export const data = [{
+    id: 0, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.33073081360451,
+    lng:24.492838046488888
+  },
+  {
+    id: 1, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.341269,
+    lng: 24.497746
+  },
+  {
+    id: 3, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.339412,
+    lng: 24.497680
+  },
+  {
+    id: 5, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.337713,
+    lng: 24.499604
+  },
+  {
+    id: 7, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.336047,
+    lng: 24.498821
+  },
+  {
+    id: 9, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.334293,
+    lng: 24.499026
+  },
+  {
+    id: 11, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.333117,
+    lng: 24.496265
+  },
+  {
+    id: 13, 
+    track_id: 1,
+    name: "Point",
+    lat: 60.331851,
+    lng: 24.493235
+  },
+  {
+    id: 14, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.302706,
+    lng: 24.551849
+  },
+  {
+    id: 15, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.303080,
+    lng: 24.555495
+  },
+  {
+    id: 16, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.301493,
+    lng: 24.557097
+  },
+  {
+    id: 17, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.301488, 
+    lng: 24.560532
+  },
+  {
+    id: 18, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.301190,
+    lng: 24.563887
+  },
+  {
+    id: 19, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.302494,
+    lng: 24.564269
+  },
+  {
+    id: 20, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.303193,
+    lng: 24.567245
+  },
+  {
+    id: 21, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.302377,
+    lng: 24.570267
+  },
+  {
+    id: 22, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.300668,
+    lng: 24.568404
+  },
+  {
+    id: 23, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.299865,
+    lng: 24.564988
+  },
+  {
+    id: 24, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.298190,
+    lng: 24.565057
+  },
+  {
+    id: 25, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.296805,
+    lng: 24.565505
+  },
+  {
+    id: 26, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.295609,
+    lng: 24.563435
+  },
+  {
+    id: 27, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.296617,
+    lng: 24.561193
+  },
+  {
+    id: 28, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.298104,
+    lng: 24.560124
+  },
+  {
+    id: 29, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.299438,
+    lng: 24.559537
+  },
+  {
+    id: 30, 
+    track_id: 0,
+    name: "Point",
+    lat: 60.300181,
+    lng: 24.557709
+  },
+  {
+    id: 31, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.27581963490531, 
+      lng: 24.46368928427852
+    },
+    {
+      id: 32, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.276988, 
+      lng: 24.466486
+    },
+    {
+      id: 33, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.278851, 
+      lng: 24.468810
+    },
+    {
+      id: 34, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.281278, 
+      lng: 24.468867
+    },
+    {
+      id: 35, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.283312, 
+      lng: 24.471084
+    },
+    {
+      id: 36, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.282891, 
+      lng: 24.476177
+    },
+    {
+      id: 37, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.282657, 
+      lng: 24.480421
+    },
+    {
+      id: 38, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.282401, 
+      lng: 24.483956
+    },
+    {
+      id: 39, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.281605, 
+      lng: 24.487728
+    },
+    {
+      id: 40, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.280717, 
+      lng: 24.491406
+    },
+    {
+      id: 41, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.279688, 
+      lng: 24.495791
+    },
+    {
+      id: 42, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.280834, 
+      lng: 24.499469
+    },
+    {
+      id: 43, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.282073, 
+      lng: 24.501591
+    },
+    {
+      id: 44, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.281698, 
+      lng: 24.505667
+    },
+    {
+      id: 45, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.281978, 
+      lng: 24.510197
+    },
+    {
+      id: 46, 
+      track_id: 2,
+      name: "Point",
+      lat: 60.283242, 
+      lng: 24.511358
+    },
+    {
+	  	id: 47, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.326924542956334, 
+        lng: 24.495358359272675
+      },
+      {
+        id: 48, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.325713, 
+        lng: 24.493192
+      },
+      {
+      	id: 49, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.323758, 
+        lng: 24.492687
+      },
+      {
+        id: 50, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.321552, 
+        lng: 24.493526
+      },
+      {
+      	id: 51, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.319845, 
+        lng: 24.496131
+      },
+      {
+        id: 52, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.318014, 
+        lng: 24.495373
+      },
+      {
+      	id: 53, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.316058, 
+        lng: 24.495540
+      },
+      {
+        id: 54, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.314142, 
+        lng: 24.494115
+      },
+      {
+      	id: 55, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.312414, 
+        lng: 24.493863
+      },
+      {
+        id: 56, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.310722, 
+        lng: 24.496162
+      },
+      {
+      	id: 57, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.309426, 
+        lng: 24.498979
+      },
+      {
+        id: 58, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.307692, 
+        lng: 24.501750
+      },
+      {
+      	id: 59, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.305753, 
+        lng: 24.504311
+      },
+      {
+        id: 60, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.305681, 
+        lng: 24.508794
+      },
+      {
+      	id: 61, 
+        track_id: 3,
+        name: "Point",
+        lat: 60.309426, 
+        lng: 24.498979
+      },
+      {
+        id: 62, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.29819039594651, 
+          lng: 24.45628974906327
+        },
+        {
+          id: 63, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.297806, 
+          lng: 24.459306
+        },
+        {
+          id: 64, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.298204, 
+          lng: 24.462875
+        },
+        {
+          id: 65, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.298453, 
+          lng: 24.466595
+        },
+        {
+          id: 66, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.297979, 
+          lng: 24.470416
+        },
+        {
+          id: 67, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.298502, 
+          lng: 24.473784
+        },
+        {
+          id: 4, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.298461, 
+          lng: 24.477171
+        },
+        {
+          id: 68, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.298900, 
+          lng: 24.480974
+  
+        },
+        {
+          id: 69, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.300341, 
+          lng: 24.481168
+        },
+        {
+          id: 70, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.301143, 
+          lng: 24.478398
+        },
+        {
+          id: 71, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.301504, 
+          lng: 24.475418
+        },
+        {
+          id: 72, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.302180, 
+          lng: 24.472049
+        },
+        {
+          id: 73, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.303291, 
+          lng: 24.471335
+        },
+        {
+          id: 74, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.304671, 
+          lng: 24.470149
+        },
+        {
+          id: 75, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.305976, 
+          lng: 24.468997
+        },
+        {
+          id: 6, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.307829, 
+          lng: 24.467358
+        },
+        {
+          id: 76, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.309501, 
+          lng: 24.466382
+        },
+        {
+          id: 77, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.310795, 
+          lng: 24.463821
+        },
+        {
+          id: 78, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.312064, 
+          lng: 24.461260
+        },
+        {
+          id: 79, 
+          track_id: 4,
+          name: "Point",
+          lat: 60.313535, 
+          lng: 24.461004
+        },
+        {
+          id: 80, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.28242199633351, 
+            lng: 24.505873789599413
+          },
+          {
+            id: 81, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.283653, 
+            lng: 24.502598
+          },
+          {
+            id: 82, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.284547, 
+            lng: 24.499801
+          },
+          {
+            id: 83, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.286861, 
+            lng: 24.499393
+          },
+          {
+            id: 84, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.288678, 
+            lng: 24.500574
+          },
+          {
+            id: 85, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.290587, 
+            lng: 24.501321
+    
+          },
+          {
+            id: 86, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.292373, 
+            lng: 24.499830
+    
+          },
+          {
+            id: 87, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.294262, 
+            lng: 24.498461
+          },
+          {
+            id: 88, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.295765, 
+            lng: 24.497145
+          },
+          {
+            id: 89, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.297382, 
+            lng: 24.494914
+          },
+          {
+            id: 90, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.299055, 
+            lng: 24.494972
+          },
+          {
+            id: 91, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.300246, 
+            lng: 24.491595
+          },
+          {
+            id: 92, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.302033, 
+            lng: 24.489821
+          },
+          {
+            id: 93, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.302854, 
+            lng: 24.493140
+          },
+          {
+            id: 94, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.303279, 
+            lng: 24.497261
+          },
+          {
+            id: 95, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.303420, 
+            lng: 24.501324
+          },
+          {
+            id: 96, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.312064, 
+            lng: 24.461260
+          },
+          {
+            id: 97, 
+            track_id: 5,
+            name: "Point",
+            lat: 60.313535, 
+            lng: 24.461004
+          }    
+   ]
--- front-end/src/screens/Chances/Chances.jsx
+++ front-end/src/screens/Chances/Chances.jsx
@@ -4,95 +4,14 @@ import styles from './Chances.module.css';
 import Marker from '../../components/Marker';
 import GoogleMap from '../../components/GoogleMap';
 
+import * as places_data from '../../data/places.js';
+
 class Chances extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      places: [{
-        id: 0, 
-        name: "Point",
-        lat: 60.33073081360451,
-        lng:24.492838046488888
-      },
-      {
-        id: 1, 
-        name: "Point",
-        lat: 60.341269,
-        lng: 24.497746
-      },
-      {
-        id: 2, 
-        name: "Point",
-        lat: 60.340301,
-        lng: 24.497327
-      },
-      {
-        id: 3, 
-        name: "Point",
-        lat: 60.339412,
-        lng: 24.497680
-      },
-      {
-        id: 4, 
-        name: "Point",
-        lat: 60.338491,
-        lng: 24.498418
-      },
-      {
-        id: 5, 
-        name: "Point",
-        lat: 60.337713,
-        lng: 24.499604
-      },
-      {
-        id: 6, 
-        name: "Point",
-        lat: 60.336665,
-        lng: 24.499893
-      },
-      {
-        id: 7, 
-        name: "Point",
-        lat: 60.336047,
-        lng: 24.498821
-      },
-      {
-        id: 8, 
-        name: "Point",
-        lat: 60.335138,
-        lng: 24.498267
-      },
-      {
-        id: 9, 
-        name: "Point",
-        lat: 60.334293,
-        lng: 24.499026
-      },
-      {
-        id: 10, 
-        name: "Point",
-        lat: 60.333674,
-        lng: 24.497712
-      },
-      {
-        id: 11, 
-        name: "Point",
-        lat: 60.333117,
-        lng: 24.496265
-      },
-      {
-        id: 12, 
-        name: "Point",
-        lat: 60.332290,
-        lng: 24.494818
-      },
-      {
-        id: 13, 
-        name: "Point",
-        lat: 60.331851,
-        lng: 24.493235
-      }]
+      places: places_data.data
     }
   }
 
