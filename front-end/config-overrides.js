0232061cbe643837963f77099241181999a47dbf
Simen Fossnes <simenf13@student.uia.no>
2019-11-17 08:16:52 +0100

    style: 💄 Fixed color scheme

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -8,6 +8,6 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#00ff99' },
+    modifyVars: { '@primary-color': '#43b549' },
   })
 );
