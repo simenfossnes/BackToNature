6688e47e154c3248d27216b0000370a83d04ee58
Simen Fossnes <simenf13@student.uia.no>
2019-11-18 13:17:51 +0100

    fix: 🐛 updated twitter faas to fix CORS

--- back-end/faas/TwitterProxy/index.js
+++ back-end/faas/TwitterProxy/index.js
@@ -5,23 +5,22 @@
  * @param {!express:Response} res HTTP response context.
  */
 
-var Twit = require("twit");
-
+var Twit = require('twit');
+ 
 var T = new Twit({
-  consumer_key: "---",
-  consumer_secret: "---",
-  access_token: "---",
-  access_token_secret: "---",
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true // optional - requires SSL certificates to be valid.
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
 });
 
 exports.tweets = (req, res) => {
-  T.get("search/tweets", { q: "#backtonature", count: 100 }, function(
-    err,
-    data,
-    response
-  ) {
-    res.status(200).send(data);
+  res.set('Access-Control-Allow-Origin', "*");
+  res.set('Access-Control-Allow-Methods', 'GET');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  T.get('search/tweets', { q: '#backtonature', count: 100 }, function(err, data, response) {
+  	res.status(200).send(data)
   });
-};
+}
